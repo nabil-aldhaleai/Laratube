e197e804dffe2dc7026880384cf169e3cfd1e4d9
Nabil Aldhaleai <naldhaleai@infotech.com>
2019-07-22 09:31:23 -0400

    Composer Update

--- public/js/app.js
+++ public/js/app.js
@@ -33198,8 +33198,8 @@ Vue.component('subscribe-button', {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\laratube\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laratube\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/naldhaleai/.Trash/Laratube/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/naldhaleai/.Trash/Laratube/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
